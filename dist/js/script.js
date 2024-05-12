2edd8fe23810143ce6efb4c0ca0f4272fb438ba0
Mark Korzin <mark.korzin@gmail.com>
2024-05-12 19:57:49 +0300

    Step 4. Finished website adaptation for mobile devices

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,23 @@
+//Hamburger
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close'),
+      link = document.querySelector('.menu__link');
+      
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+link.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+//Slider
+
 const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
@@ -12,4 +32,30 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
+});
+
+//Modal windows closing function
+
+$('.modal__close').on('click', function() {
+    $('.overlay').fadeOut('slow');
+});
+
+//Form sending
+$('#form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #thanks').fadeIn('slow');
+        $('#form').trigger('reset');
+    });
+    return false;
 });
